491407730cf7ec0102b1760a028504dbb1656d9d
Amit Goyal <goyal.amit2@gmail.com>
2016-02-25 11:07:48 -0600

    almost there

---
+++ js/app.js
@@ -0,0 +1,6 @@
+window.onload = function (event) {
+  // anything called or done isideof here will only occur after thepage has
+  // loaded every image, file style, etc.
+
+console.log('we made it')
+};
